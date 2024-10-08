b267396de85c3a9c643bd40a1c7fe95063ed12ec
AD <154968000+dangol-anish@users.noreply.github.com>
2024-10-08 20:57:09 +0545

    add: separator ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,19 @@
 import React, { useState } from "react";
 import AddPrompt from "@/components/AddPrompt";
 import ResumeReport from "@/components/ResumeReport";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   const [evaluations, setEvaluations] = useState<string[]>([]);
 
   return (
     <>
-      <main className="flex w-[100%]">
+      <main className="flex w-[100%] ">
         <AddPrompt setEvaluations={setEvaluations} />
+        <Separator
+          className="w-[1px] h-screen bg-black/10"
+          orientation="horizontal"
+        />
         <ResumeReport evaluations={evaluations} />
       </main>
     </>
--- components/ResumeReport.tsx
+++ components/ResumeReport.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 export interface ResumeReportProps {
-  evaluations: string[]; // Changed to accept an array of strings
+  evaluations: string[];
 }
 
 function ResumeReport({ evaluations }: ResumeReportProps) {
   return (
-    <div className="flex h-full overflow-y-auto justify-center flex-col">
+    <div className="flex h-full overflow-y-auto justify-center flex-col ">
       <h2 className="text-lg font-bold">Resume Report</h2>
-      {evaluations.length > 0 ? ( // Check if there are evaluations to display
+      {evaluations.length > 0 ? (
         evaluations.map((evaluation, index) => (
           <div key={index} className="mt-4 p-4 border rounded shadow">
             <h3 className="text-lg font-semibold">
