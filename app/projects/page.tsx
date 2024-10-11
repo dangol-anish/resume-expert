d100e71e4c9a6ffacee8252e835ae12fa801386a
AD <154968000+dangol-anish@users.noreply.github.com>
2024-10-11 12:17:18 +0545

    update: layout for card component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,13 +2,16 @@ import { HeadingItem } from "@/data";
 import React from "react";
 import { Brygada_1918 } from "next/font/google";
 import Link from "next/link";
+import { CardComponent } from "@/data";
+import Image from "next/image";
+import card from "../../public/card.svg";
 
 const brygada = Brygada_1918({ subsets: ["latin"] });
 
 function Projects() {
   return (
-    <div className="h-full flex flex-col w-full p-8 text-primary_color items-center gap-12">
-      <div className="flex flex-col items-center ">
+    <div className="h-full flex flex-col w-full p-8 text-primary_color items-center gap-8">
+      <div className="flex flex-col items-center">
         {HeadingItem[0].items.map((item, index) => (
           <div key={index}>
             {item.title && (
@@ -20,33 +23,47 @@ function Projects() {
             )}
             {item.main_heading && (
               <h2 className="text-3xl font-semibold mb-1 tracking-tight">
-                {" "}
                 {item.main_heading}
               </h2>
             )}
             {item.sub_heading && (
               <p className="text-sm text-primary_color/70 tracking-wide">
-                {" "}
                 {item.sub_heading}
               </p>
             )}
           </div>
         ))}
       </div>
-      <div className="h-full w-full flex flex-col">
-        <div className="flex-grow px-8 py-4 grid grid-cols-3 gap-4">
-          <div className=" p-4 text-center">
-            <div>box</div>
-            <p>Job Title</p>
-            <p>10 jobs</p>
-          </div>
-          <div className=" p-4 text-center">2</div>
-          <div className=" p-4 text-center">3</div>
-          <div className=" p-4 text-center">4</div>
-          <div className=" p-4 text-center">5</div>
-          <div className=" p-4 text-center">6</div>
-        </div>
 
+      <div className="h-full w-full flex flex-col justify-between">
+        {CardComponent.length > 0 ? (
+          <div className="flex-grow px-8 py-4 grid grid-cols-3 gap-4 max-h-[50vh] overflow-y-auto no-scrollbar">
+            {CardComponent.map((item) => (
+              <div
+                key={item.id}
+                className="flex flex-col items-center text-a_black"
+              >
+                <Image src={card} alt="Card" />{" "}
+                <p className="text-sm">{item.job_title}</p>
+                <p className="text-xs text-a_black/70">
+                  {item.no_of_jobs} Jobs
+                </p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="w-full flex justify-center items-center mt-4 h-full">
+            <Link
+              href="/projects/add"
+              className="flex justify-center items-center bg-[#FFB158] text-a_black border border-primary_color rounded-full font-bold hover:bg-primary_color px-5 py-2 h-"
+            >
+              Create a project
+            </Link>
+          </div>
+        )}
+      </div>
+
+      {CardComponent.length === 0 ? null : (
         <div className="w-full flex justify-center mt-4">
           <Link
             href="/projects/add"
@@ -55,7 +72,7 @@ function Projects() {
             Create a project
           </Link>
         </div>
-      </div>
+      )}
     </div>
   );
 }
