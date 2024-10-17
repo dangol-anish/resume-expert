eb3111e668f93b69a08b96d061e3d4d8afaa42ac
AD <154968000+dangol-anish@users.noreply.github.com>
2024-10-17 17:00:13 +0545

    update: middleware auth for protected routes

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -2,6 +2,8 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function updateSession(request: NextRequest) {
+  const url = request.nextUrl.clone();
+
   let response = NextResponse.next({
     request: {
       headers: request.headers,
@@ -54,7 +56,30 @@ export async function updateSession(request: NextRequest) {
     }
   );
 
-  await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    if (
+      !url.pathname.startsWith("/login") &&
+      !url.pathname.startsWith("/signup") &&
+      url.pathname !== "/"
+    ) {
+      url.pathname = "/login";
+      return NextResponse.redirect(url);
+    }
+  } else {
+    if (
+      url.pathname.startsWith("/login") ||
+      url.pathname.startsWith("/signup") ||
+      url.pathname === "/"
+    ) {
+      // user is logged in but trying to access login, signup, or root page, redirect to a protected page
+      url.pathname = "/projects";
+      return NextResponse.redirect(url);
+    }
+  }
 
   return response;
 }
